9a77fc63468c5e171168d79d76fbc16792b8776e
Quang Duy <duy2001x1@gmail.com>
2017-11-26 09:23:16 +0700

    initial commit

--- session2/homework/exercise4e.py
+++ session2/homework/exercise4e.py
@@ -1,11 +1,4 @@
-print('''x * x * x * x * x *
-* x * x * x * x * x
-x * x * x * x * x *
-* x * x * x * x * x
-x * x * x * x * x *
-* x * x * x * x * x
-x * x * x * x * x *
-* x * x * x * x * x
-x * x * x * x * x *
-* x * x * x * x * x
-''')
+for i in range(10):
+    for a in range(10):
+        print("x * ", end = "")
+    print()
